83b7cf8766231e61120420dfa3db16dde2d3b6cc
lesssliemarie <lesssliemarie@gmail.com>
2014-03-24 13:32:49 -0500

    Created new view that displays random die roll

---
+++ app/views/roll-dice.php
@@ -0,0 +1,22 @@
+<?php
+	
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Roll Dice View</title>
+
+<style>
+body {
+	text-align: center;
+}
+</style>
+
+</head>
+<body>
+	<h1>Rolled Number:</h1>
+	<h2><?= $roll; ?></h2>
+
+</body>
+</html>
