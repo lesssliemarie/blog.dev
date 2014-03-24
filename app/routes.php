9fb30402f699127c7943c964a45f644500e1589f
lesssliemarie <lesssliemarie@gmail.com>
2014-03-24 11:35:49 -0500

    Added period to string in route exercise

--- app/routes.php
+++ app/routes.php
@@ -30,10 +30,10 @@ Route::get('/sayhello/{name}', function($name)
 
 Route::get('/resume', function()
 {
-	return 'This is my resume';
+	return 'This is my resume.';
 });
 
 Route::get('/portfolio', function()
 {
-	return 'This is my portfolio';
+	return 'This is my portfolio.';
 });
