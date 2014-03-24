eafd9e8efecd21e909b902407f490770611a7216
lesssliemarie <lesssliemarie@gmail.com>
2014-03-24 13:43:13 -0500

    Added if statement to check if roll and guess are the same

--- app/views/roll-dice.php
+++ app/views/roll-dice.php
@@ -18,6 +18,12 @@ body {
 	<h1>Rolled Number:</h1>
 	<h2><?= $roll; ?></h2>
 
+	<? if ($roll === $guess): ?>
+	<h3>LUCKY GUESS!</h3>
+	<? else: ?>
+	<h3>WRONG GUESS</h3>
+	<? endif; ?>
+
 	<h1>Guessed Number:</h1>
 	<h2><?= $guess; ?></h2>
 
