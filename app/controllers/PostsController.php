4678accdcf85cece83004ccef43450631cf7c3bb
lesssliemarie <lesssliemarie@gmail.com>
2014-03-25 14:47:39 -0500

    Added log to store to log all inputs

--- app/controllers/PostsController.php
+++ app/controllers/PostsController.php
@@ -31,6 +31,7 @@ class PostsController extends \BaseController {
 	public function store()
 	{
 		// return 'POST, Stores the new post';
+		Log::info(Input::all());
 		return Redirect::back()->withInput();
 	}
 
