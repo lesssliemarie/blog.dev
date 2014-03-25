fa7bdc1bf616eb6e1ac7f0a88ec667a7f5dfaa57
lesssliemarie <lesssliemarie@gmail.com>
2014-03-25 13:49:42 -0500

    Added value attribute to show old input

--- app/views/posts/create.blade.php
+++ app/views/posts/create.blade.php
@@ -4,11 +4,11 @@
 	<form role="form" method="POST" action="{{{ action('PostsController@store') }}}">
 		<div class="form-group">
 			<label for="title">Title:</label>
-			<input type="text" class="form-control" id="title" placeholder="Your title here...">
+			<input type="text" class="form-control" id="title" placeholder="Your title here..." value="{{{ Input::old('title') }}}">
 		</div>
 		<div class="form-group">
 			<label for="body">Body:</label>
-			<input type="text" class="form-control" id="body" placeholder="Type here...">
+			<input type="text" class="form-control" id="body" placeholder="Type here..." value="{{{ Input::old('body') }}}">
 		</div>
 	 </form>
 @stop
