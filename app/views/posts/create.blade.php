ecfba05cb748fec288d761134f4c58083db0b44e
lesssliemarie <lesssliemarie@gmail.com>
2014-03-25 13:55:42 -0500

    Added name attribute

--- app/views/posts/create.blade.php
+++ app/views/posts/create.blade.php
@@ -4,11 +4,12 @@
 	<form role="form" method="POST" action="{{{ action('PostsController@store') }}}">
 		<div class="form-group">
 			<label for="title">Title:</label>
-			<input type="text" class="form-control" id="title" placeholder="Your title here..." value="{{{ Input::old('title') }}}">
+			<input type="text" class="form-control" id="title" name="title" placeholder="Your title here..." value="{{{ Input::old('title') }}}">
 		</div>
 		<div class="form-group">
 			<label for="body">Body:</label>
-			<input type="text" class="form-control" id="body" placeholder="Type here..." value="{{{ Input::old('body') }}}">
+			<input type="text" class="form-control" id="body" name="body" placeholder="Type here..." value="{{{ Input::old('body') }}}">
 		</div>
+		<button class="btn">Submit</button>
 	 </form>
 @stop
