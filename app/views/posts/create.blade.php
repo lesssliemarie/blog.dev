7be72d657559b4e6880390d662f8e4bec9db9dca
lesssliemarie <lesssliemarie@gmail.com>
2014-03-25 16:14:19 -0500

    Changed button to say Create Post instead of Submit

--- app/views/posts/create.blade.php
+++ app/views/posts/create.blade.php
@@ -18,7 +18,7 @@
 		</div>
 		<div class="form-group">
 			<div class="col-sm-offset-2 col-sm-10">
-				<button class="btn">Submit</button>
+				<button class="btn">Create Post</button>
 			</div>
 		</div>
 	 </form>
