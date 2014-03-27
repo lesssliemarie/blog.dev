6a09d8b40777732a209da27d4817fbc7b26c133e
lesssliemarie <lesssliemarie@gmail.com>
2014-03-27 15:05:53 -0500

    Used form builder to create form

--- app/views/posts/create.blade.php
+++ app/views/posts/create.blade.php
@@ -3,26 +3,30 @@
 @section('content')
 	<h1 class="cursive">Create a New Post</h1>
 	<hr>
-	<form class="form-horizontal" role="form" method="POST" action="{{{ action('PostsController@store') }}}">
-		<div class="form-group">
-			<label for="title" class="col-sm-2 control-label">Title:</label>
-			<div class="col-sm-10">
-				<input type="text" class="form-control" id="title" name="title" placeholder="Your title here..." value="{{{ Input::old('title') }}}">
-				{{ $errors->has('title') ? $errors->first('title', '<p><span class="help-block">:message</span></p>') : '' }}
-			</div>
+
+<!-- START FORM -->
+	{{ Form::open(array('action' => 'PostsController@store', 'class' => 'form-horizontal')) }}
+	<div class="form-group">
+		{{ Form::label('title', 'Title', array('class' => 'col-sm-2 control-label')) }}
+		<div class="col-sm-10">
+			{{ Form::text('title', '', array('class' => 'form-control')) }}
+			{{ $errors->has('title') ? $errors->first('title', '<p><span class="help-block">:message</span></p>') : '' }}
 		</div>
-		<div class="form-group">
-			<label for="body" class="col-sm-2 control-label">Body:</label>
-			<div class="col-sm-10">
-				<textarea class="form-control" id="body" name="body" rows="5">{{{ Input::old('body') }}}</textarea>
-				{{ $errors->has('body') ? $errors->first('body', '<p><span class="help-block">:message</span></p>') : '' }}
-			</div>
+	</div>
+	<div class="form-group">
+		{{ Form::label('body', 'Body', array('class' => 'col-sm-2 control-label')) }}
+		<div class="col-sm-10">
+			{{ Form::textarea('body', '', array('class' => 'form-control')) }}
+			{{ $errors->has('body') ? $errors->first('body', '<p><span class="help-block">:message</span></p>') : '' }}
 		</div>
-		<div class="form-group">
-			<div class="col-sm-offset-2 col-sm-10">
-				<button class="btn" type="submit">Create Post</button>
-				<a href="{{{ action('PostsController@index') }}}" class="btn btn-primary" role="button"><span class="glyphicon glyphicon-arrow-left"></span> Back to all posts</a>
-			</div>
+	</div>
+	<div class="form-group">
+		<div class="col-sm-offset-2 col-sm-10">
+			{{ Form::submit('Create Post', array('class' => 'btn btn-primary'))}}
+			<a href="{{{ action('PostsController@index') }}}" class="btn btn-primary" role="button"><span class="glyphicon glyphicon-arrow-left"></span> Back to all posts</a>
 		</div>
-	 </form>
+	</div>
+
+	{{ Form::close() }}
+<!-- END FORM -->
 @stop
