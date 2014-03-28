5d1cd3419aaf4562b809219dc4099cae1a4c2ed6
lesssliemarie <lesssliemarie@gmail.com>
2014-03-28 13:44:58 -0500

    Added flash error messages

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -38,7 +38,13 @@
 
     <!-- UNIQUE PAGE CONTENT --> 
     <div class="container">
-    	@yield('content')
+    	@if (Session::has('successMessage'))
+        <div class="alert alert-success">{{{ Session::get('successMessage') }}}</div>
+      @endif
+      @if (Session::has('errorMessage'))
+        <div class="alert alert-danger">{{{ Session::get('errorMessage') }}}</div>
+      @endif
+      @yield('content')
     </div>
 
     <!-- FOOTER -->
