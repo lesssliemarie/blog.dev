14cedaa1570296e5dc3b66d186194d941de6da51
lesssliemarie <lesssliemarie@gmail.com>
2014-03-28 13:46:01 -0500

    Fixed indentation

--- app/views/posts/index.blade.php
+++ app/views/posts/index.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.master')
 
 @section('content')
-	<div id="blog">
+    <div id="blog">
         <h1 class="cursive">This will be my blog.</h1>
         <hr>
         <div class="row">
