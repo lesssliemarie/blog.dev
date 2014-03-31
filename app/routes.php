5ca28309a2201dca265ab15a44f6582607f42047
lesssliemarie <lesssliemarie@gmail.com>
2014-03-31 16:02:23 -0500

    Removed exercise routes

--- app/routes.php
+++ app/routes.php
@@ -22,34 +22,3 @@ Route::get('/blog', 'HomeController@showBlog');
 Route::get('/resume', 'HomeController@showResume');
 
 Route::resource('/posts', 'PostsController');
-
-// Exercise routes
-
-// Route::get('/', function()
-// {
-// 	return View::make('hello');
-// });
-
-Route::get('/sayhello/{name}', function($name)
-{
-    $data = array(
-    	'name' => $name
-    );
-
-    return View::make('my-first-view')->with($data);
-});
-
-Route::get('/rolldice/{guess?}', function($guess = 3)
-{
-	$roll = rand(1, 6);
-	
-	$message = ($roll == $guess) ? 'LUCKY GUESS!' : 'WRONG GUESS!';
-
-	$data = array(
-		'roll' => $roll,
-		'guess' => $guess,
-		'message' => $message
-	);
-	return View::make('roll-dice')->with($data);
-
-});
