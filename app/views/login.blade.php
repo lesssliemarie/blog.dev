549c27a1e1bff6fe603c70682547f604eaa3023a
lesssliemarie <lesssliemarie@gmail.com>
2014-04-01 15:14:44 -0500

    Changed form action for login to doLogin

--- app/views/login.blade.php
+++ app/views/login.blade.php
@@ -6,19 +6,17 @@
 
 	<div class="col-md-5">
 <!-- START FORM -->
-	{{ Form::open(array('action' => 'PostsController@store', 'class' => 'form-horizontal')) }}
+	{{ Form::open(array('action' => 'HomeController@doLogin', 'class' => 'form-horizontal')) }}
 	<div class="form-group">
 		{{ Form::label('email', 'Email', array('class' => 'col-sm-2 control-label')) }}
 		<div class="col-sm-10">
 			{{ Form::text('email', null, array('class' => 'form-control')) }}
-			{{ $errors->first('email', '<p><span class="help-block">:message</span></p>') }}
 		</div>
 	</div>
 	<div class="form-group">
 		{{ Form::label('password', 'Password', array('class' => 'col-sm-2 control-label')) }}
 		<div class="col-sm-10">
 			{{ Form::text('password', null, array('class' => 'form-control')) }}
-			{{ $errors->first('password', '<p><span class="help-block">:message</span></p>') }}
 		</div>
 	</div>
 	<div class="form-group">
