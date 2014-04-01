20e3cb8338c803e453d0fca8643591d8550567ff
lesssliemarie <lesssliemarie@gmail.com>
2014-04-01 16:11:36 -0500

    Created accessDenied view

---
+++ app/views/accessDenied.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.master')
+
+@section('content')
+	<h1 class="cursive"><i class="fa fa-ban"></i> ACCESS DENIED</h1>
+	<hr>
+	<p>You do not have permission to take that action. Sorry!</p>
+@stop
