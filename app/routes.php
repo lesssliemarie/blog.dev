b42f41ce57076a491e26ec3c5bf139df83fe1ffc
lesssliemarie <lesssliemarie@gmail.com>
2014-04-01 15:00:25 -0500

    Fixed spelling error

--- app/routes.php
+++ app/routes.php
@@ -25,6 +25,6 @@ Route::get('/login', 'HomeController@showLogin');
 
 Route::post('/login', 'HomeController@doLogin');
 
-oute::get('/logout', 'HomeController@logout');
+Route::get('/logout', 'HomeController@logout');
 
 Route::resource('/posts', 'PostsController');
