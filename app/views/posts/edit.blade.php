9f32f4a41d2a828475333ef6f7a3cb0fb081b1b0
lesssliemarie <lesssliemarie@gmail.com>
2014-04-02 15:10:26 -0500

    Added image upload and preview to edit view

--- app/views/posts/edit.blade.php
+++ app/views/posts/edit.blade.php
@@ -5,7 +5,7 @@
 	<hr>
 
 <!-- START FORM -->
-	{{ Form::model($post, array('action' => array('PostsController@update', $post->id), 'method' => 'PUT', 'class' => 'form-horizontal')) }}
+	{{ Form::model($post, array('action' => array('PostsController@update', $post->id), 'method' => 'PUT', 'files' => true, 'class' => 'form-horizontal')) }}
 	<div class="form-group">
 		{{ Form::label('title', 'Title', array('class' => 'col-sm-2 control-label')) }}
 		<div class="col-sm-10">
@@ -18,6 +18,21 @@
 		<div class="col-sm-10">
 			{{ Form::textarea('body', null, array('class' => 'form-control')) }}
 			{{ $errors->first('body', '<p><span class="help-block">:message</span></p>') }}
+		</div>
+	</div>
+	@if ($post->image)
+	<div class="form-group">
+		<label class="col-sm-2 control-label">Current Photo:</label>
+		<div class="col-sm-10">
+			<img src="{{ $post->image }}"></p>
+		</div>
+	</div>
+	@else
+	@endif
+	<div class="form-group">
+		{{ Form::label('image', 'Upload Image', array('class' => 'col-sm-2 control-label')) }}
+		<div class="col-sm-10">
+			{{ Form::file('image', array('class' => 'form-control')) }}
 		</div>
 	</div>
 	<div class="form-group">
