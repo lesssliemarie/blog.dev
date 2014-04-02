5d39391830aa40833c78d1f01fb236258fc6736f
lesssliemarie <lesssliemarie@gmail.com>
2014-04-02 11:05:06 -0500

    Added username to UserTableSeeder

--- app/database/seeds/UserTableSeeder.php
+++ app/database/seeds/UserTableSeeder.php
@@ -7,11 +7,13 @@ class UserTableSeeder extends Seeder {
 		DB::table('users')->delete();
 
 		$user = new User();
+		$user->username = 'leslie';
 		$user->email = 'leslie@codeup.com';
 		$user->password = 'letmein';
 		$user->save();
 
 		$user = new User();
+		$user->username = 'jason';
 		$user->email = 'jason@codeup.com';
 		$user->password = 'letmeintoo';
 		$user->save();
