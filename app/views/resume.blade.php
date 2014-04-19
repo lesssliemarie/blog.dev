3d5ecaccd62503de6bc0a1f630db8de5009bc384
lesssliemarie <lesssliemarie@gmail.com>
2014-04-19 17:15:04 -0500

    Edited styling

--- app/views/resume.blade.php
+++ app/views/resume.blade.php
@@ -5,7 +5,7 @@
         <h1 class="cursive">Resume</h1>
         <hr>
       </div>
-      <div class="row">
+      <div class="row resume-row">
           <div class="col-md-4">
             CONTACT
           </div>
@@ -20,7 +20,7 @@
           </div>
       </div>
       <hr>
-      <div class="row">
+      <div class="row resume-row">
           <div class="col-md-4">
             SKILLS
           </div>
@@ -28,15 +28,15 @@
             <strong>Web Development</strong><br>
             HTML/CSS, JavaScript, JQuery, PHP, MySQL<br><br>
             <strong>Graphic Design</strong><br>
-            Adobe Creative Suite, Identity and Branding, Copywriting, Advertising, Print, Web, UI<br><br>
+            Adobe Creative Suite, Identity and Branding, Copywriting, Advertising, Print, Web, UI, UX<br><br>
             <strong>Administrative</strong><br>
-            80wpm, Technical Writing, Proofreading and Editing, MS Word, Excel, Access, Outlook, Powerpoint
+            Technical Writing, Proofreading and Editing, MS Word, Excel, Access, Outlook, Powerpoint
           </div>
           <div class="col-md-3">
           </div>
       </div>
       <hr>
-      <div class="row">
+      <div class="row resume-row">
           <div class="col-md-4">
             WORK EXPERIENCE
           </div>
@@ -47,22 +47,47 @@
             Time to Time<br>
           </div>
       </div>
+      <div class="row resume-row">
+          <div class="col-md-4"></div>
+          <div class="col-md-5">
+            <strong>Job 2</strong>
+          </div>
+          <div class="col-md-3">
+            Time to Time<br>
+          </div>
+      </div>
+      <div class="row resume-row">
+          <div class="col-md-4"></div>
+          <div class="col-md-5">
+            <strong>Job 2</strong>
+          </div>
+          <div class="col-md-3">
+            Time to Time<br>
+          </div>
+      </div>
       <hr>
-      <div class="row">
+      <div class="row resume-row">
           <div class="col-md-4">
             EDUCATION
           </div>
           <div class="col-md-5">
             <strong>LAMP+J Software Development Bootcamp</strong><br>
             <a href="http://www.codeup.com/">Codeup</a><br>
-            <em>San Antonio, Texas</em><br><br>
+            <em>San Antonio, Texas</em>
+          </div>
+          <div class="col-md-3">
+            Graduating Spring 2014<br>
+            <span class="label label-success">IN PROGRESS</span>
+          </div>
+      </div>
+      <div class="row resume-row">
+          <div class="col-md-4"></div>
+          <div class="col-md-5">
             <strong>BFA Communication Design</strong><br>
             Texas State University<br>
             <em>San Marcos, Texas</em>
           </div>
           <div class="col-md-3">
-            Graduating Spring 2014<br>
-            <span class="label label-success">IN PROGRESS</span><br><br><br>
             Fall 2007 - Fall 2010
           </div>
       </div>
